45b6224e7226963164475fc9bff932e8725d8638
FlacoRD <rodriguezlopezmiguelangel77@gmail.com>
2024-08-27 21:50:55 -0600

    Agregar archivos de proyecto.

---
+++ 3KLSC-MiguelRodriguez-03.c+/FileName.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <iostream>
+
+#define maxf 3
+#define maxc 5
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    float a[maxf][maxc];
+    int f, c;
+
+    //leer el array
+    for (f = 0; f < maxf; f++)
+    {
+        for (c = 0; c < maxc; c++) {
+            cin >> a[f][c];
+        }
+    }
+
+    //escribir el array
+    for (f = 0; f < maxf; f++)
+    {
+        for (c = 0; c < maxc; c++) {
+            cout << a[f][c];
+            cout << endl;
+        }
+    }
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
